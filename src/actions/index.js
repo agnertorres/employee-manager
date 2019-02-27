e0dc508dbd616c4e4cfcc5df29ddad5587a0e602
Agner Torres <agnertorres@gmail.com>
2019-02-27 18:13:23 -0300

    autenticaçao com firebase

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+
+console.log(firebase);
 
 export const emailChanged = (text) => {
   return {
@@ -12,4 +15,13 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   }
+};
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user});
+      });
+  };
 };
